150bf0b458c2cd830e31502860a206c4055dec96
Ben <benakehurst@hotmail.com>
2021-01-16 16:22:45 +0000

    inital commit

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './NavigationItems.scss';
+import NavigationItem from './NavigationItem/NavigationItem';
+
+const NavigationItems = (props) => (
+  <ul className="NavigationItems">
+    <NavigationItem link="/" exact>
+      Burger Builder
+    </NavigationItem>
+    {props.isAuthenticated ? (
+      <NavigationItem link="/orders">Orders</NavigationItem>
+    ) : null}
+    {!props.isAuthenticated ? (
+      <NavigationItem link="/auth">Authenticate</NavigationItem>
+    ) : (
+      <NavigationItem link="/logout">Logout</NavigationItem>
+    )}
+  </ul>
+);
+
+export default NavigationItems;
---
+++ src/components/UI/Spinner/Spinner.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './Spinner.scss';
+
+const Spinner = () => <div className="Loader">Loading...</div>;
+
+export default Spinner;
