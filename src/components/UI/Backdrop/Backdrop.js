b890359097e311742d1f88f02a5def9eb458455f
Ben <benakehurst@hotmail.com>
2021-01-21 16:25:28 +0000

    remove project

--- src/components/UI/Backdrop/Backdrop.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-import './Backdrop.scss';
-
-const Backdrop = (props) =>
-  props.show ? <div className="Backdrop" onClick={props.clicked}></div> : null;
-
-export default Backdrop;
--- src/components/UI/Spinner/Spinner.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import './Spinner.scss';
-
-const Spinner = () => <div className="Loader">Loading...</div>;
-
-export default Spinner;
